88396751c62a42a554a5d228a3747755ffabdf1f
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-11-22 14:19:05 +0200

    update deployment configuration and add GitHub Actions workflow for automatic deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-real-estate-app/home",
+  base: "/react-real-estate-app/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
