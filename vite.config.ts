6030ebb3a2881e1bcee493598193567a06d61f0e
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-11-22 14:08:41 +0200

    update base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-real-estate-app/",
+  base: "/react-real-estate-app/home",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
